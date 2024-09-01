a8495aab7f572d486f0579f882d19fc4b214859f
Fermin Da Cruz <ferminjuliancruz26@gmail.com>
2024-09-01 15:17:43 -0300

    error swagger

---
+++ authservice/src/main/java/com/example/authservice/common/dtos/UserRequest.java
@@ -0,0 +1,17 @@
+package com.example.authservice.common.dtos;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequest {
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+
+}
